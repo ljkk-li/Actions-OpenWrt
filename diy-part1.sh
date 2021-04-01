4543f9f085639da8680062b0b9d79da5cee59e25
ljkk-li <51225555+ljkk-li@users.noreply.github.com>
2021-04-01 17:55:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#Remove packages
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git 
-#git clone https://github.com/281677160/openwrt-package package/danshui
+
+git clone https://github.com/281677160/openwrt-package package/danshui
