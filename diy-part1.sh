115c801cd4437bcd2cfcafaef63a1e79a71885e7
ljkk-li <51225555+ljkk-li@users.noreply.github.com>
2025-11-08 20:43:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-svn co https://github.com/fw876/helloworld/trunk/shadowsocks-libev feeds/small/shadowsocks-libev
+
 
 
 
